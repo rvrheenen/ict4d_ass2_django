72ba7e7895af29783bcf833332888f3c38b83c0a
rvrheenen <rick.van.rheenen@gmail.com>
2018-04-23 21:18:37 +0200

    Tutorial part 7: Customize adminpages

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Polls Administration"
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls)
